6f73651d23a185354aacf70ea9b619894ee1d19e
fmarcinek <fmarcinek15@gmail.com>
2020-10-25 20:46:16 +0100

    Fix payments.py indentation

--- tickets_component/payments_adapter/payments.py
+++ tickets_component/payments_adapter/payments.py
@@ -19,13 +19,12 @@ PaymentResult = namedtuple('PaymentResult', ('amount', 'currency'))
 class PaymentGateway:
     supported_currencies = ('EUR',)
 
-
-def charge(self, amount, token, currency='EUR'):
-    if token == 'card_error':
-        raise CardError("Your card has been declined")
-    elif token == 'payment_error':
-        raise PaymentError("Something went wrong with your transaction")
-    elif currency not in self.supported_currencies:
-        raise CurrencyError(f"Currency {currency} not supported")
-    else:
-        return PaymentResult(amount, currency)
+    def charge(self, amount, token, currency='EUR'):
+        if token == 'card_error':
+            raise CardError("Your card has been declined")
+        elif token == 'payment_error':
+            raise PaymentError("Something went wrong with your transaction")
+        elif currency not in self.supported_currencies:
+            raise CurrencyError(f"Currency {currency} not supported")
+        else:
+            return PaymentResult(amount, currency)
